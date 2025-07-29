b2ee299f8f8329ac430b654aac7a30b2060c4fa6
Your Name <you@example.com>
2025-07-29 18:44:10 +0000

    Upload Task-3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Type-annotated function """
+
+
+def to_str(n: float) -> str:
+    """The function takes one type-annotated float argument,
+        and returns the str """
+    return str(n)
