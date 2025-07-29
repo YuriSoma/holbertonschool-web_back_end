331976584cd821b310e670c252422629f6d9b355
Your Name <you@example.com>
2025-07-29 19:34:37 +0000

    Upload Task-8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -4,8 +4,9 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """ Return Callable function """
     def multiplier_function(n: float) -> float:
-        """The function takes one mixed type-annotated List[] argument,
-            and returns the float """
+        """The function takes one argument,
+            and returns the square by called argument """
         return n * multiplier
     return multiplier_function
