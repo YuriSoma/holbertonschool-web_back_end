607728fb83bc197d091f23c4f5478ad94e2bb447
Your Name <you@example.com>
2025-07-29 19:13:13 +0000

    Upload Task-5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -4,4 +4,6 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """The function takes one type-annotated List[] argument,
+        and returns the float """
     return sum(input_list)
