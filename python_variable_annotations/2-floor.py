45a61aed18961f7e5581c7ee6cd7a778d1bdecd9
Your Name <you@example.com>
2025-07-29 18:32:16 +0000

    Upload Task-2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -3,7 +3,7 @@ import math
 """ Type-annotated function """
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """The function takes one type-annotated floor argument,
         and returns the floor """
     return math.floor(n)
