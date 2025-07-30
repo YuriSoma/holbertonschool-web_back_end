a73468892727c301fc97bdf4c8bd8f995d14def5
YuriSoma <106472235+YuriSoma@users.noreply.github.com>
2025-07-30 23:04:39 +0300

    Create 1-async_comprehension.py

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function async_comprehension that takes no arguments"""
+from typing import List
+from importlib import import_module as using
+async_generator = using('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Collects 10 random numbers from an asynchronous generator."""
+    return [i async for i in async_generator()]
