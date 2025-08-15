f3ff424001f3dcc687b704f130e83db933a7a1d2
Your Name <you@example.com>
2025-08-15 15:30:13 +0000

    Upload Task-0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Function returns index range """
 
+
 def index_range(page, page_size):
     """ receives page number and page size """
     return ((page - 1) * page_size, page * page_size)
