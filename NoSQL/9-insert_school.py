b44f92160ec074e27cda2a59f54c95f2e2e9eb77
YuriSoma <106472235+YuriSoma@users.noreply.github.com>
2025-08-17 01:27:21 +0300

    Create 9-insert_school.py

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Insert document """
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ Returns _id
+    """
+    new_school = mongo_collection.insert_one(kwargs)
+
+    return (new_school.inserted_id)
