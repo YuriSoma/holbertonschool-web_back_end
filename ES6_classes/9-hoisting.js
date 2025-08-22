cc69bf269b01ee1d56027f094493fba92dbef22c
YuriSoma <106472235+YuriSoma@users.noreply.github.com>
2025-08-22 22:26:12 +0300

    Update 9-hoisting.js

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -17,7 +17,7 @@ export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass; // fix: use correct variable
+    this._holbertonClass = holbertonClass; // pass the correct HolbertonClass
   }
 
   get fullName() {
@@ -33,16 +33,13 @@ export class StudentHolberton {
   }
 }
 
-// Create classes
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-// Create students
-export const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-export const student2 = new StudentHolberton('John', 'Doe', class2020);
-export const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-export const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-export const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// List of students
 export const listOfStudents = [student1, student2, student3, student4, student5];
