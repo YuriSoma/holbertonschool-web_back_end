8291108e44e6d7aad8f6130616dc0fa46ad41b02
YuriSoma <106472235+YuriSoma@users.noreply.github.com>
2025-08-31 19:30:34 +0300

    Update 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -6,12 +6,4 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     signUpUser(firstName, lastName),
     uploadPhoto(fileName)
   ]);
-}import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
-
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
-  ]);
 }
